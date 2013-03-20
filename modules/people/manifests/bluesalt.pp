20cf1a678ae33f3718da7604942b160aa803f56f
Jeff Li <jafelee@gmail.com>
2013-03-20 14:58:54 +0800

    configure git

--- modules/people/manifests/bluesalt.pp
+++ modules/people/manifests/bluesalt.pp
@@ -1,3 +1,25 @@
 class people::bluesalt{
   include iterm2::stable
+  
+  include git
+
+  git::config::global { 'alias.ci':
+    value => 'commit'
+  }
+  git::config::global { 'alias.br':
+    value => 'branch'
+  }
+  git::config::global { 'alias.st':
+    value => 'status'
+  }
+  git::config::global { 'alias.co':
+    value => 'checkout'
+  }
+
+  git::config::global { 'core.editor':
+    value => 'vim'
+  }
+  git::config::global { 'core.ui':
+    value => 'true'
+  }
 }
